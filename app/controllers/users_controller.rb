2b1221084a72201248bab201a24fd65740ad73c9
AlexDrumia <69897831+AlexDrumia@users.noreply.github.com>
2020-08-21 17:43:57 +0200

    added user model and additional actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,41 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
-  end
-
-  def create
-    render json: params
+    render json: User.all 
   end
 
   def show
-    render json: params
+    user = User.find(params[:id])
+    
+    render json: user
+  end
+
+  def create
+    user = User.new(user_params)
+    
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end  
+  end  
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
