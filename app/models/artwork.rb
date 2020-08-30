b0fcb0eb2c4554e9535411025fbc5b4e2a474beb
AlexDrumia <69897831+AlexDrumia@users.noreply.github.com>
2020-08-30 15:08:51 +0200

    Fix Artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,9 +1,9 @@
 class Artwork < ApplicationRecord
-  validates :title, presence: true, uniqueness: { scope: :artist_id, message: "a single user cannot have two artworks with the same title" }
+  validates :title, presence: true, uniqueness: { scope: :artist_id, message: "of artwork already exists for this user. A single user cannot have two artworks with the same title" }
   validates :image_url, presence: true
   validates :artist_id, presence: true
 
-  belongs_to :user,
+  belongs_to :artist,
     primary_key: :id,
     foreign_key: :artist_id,
     class_name: :User
