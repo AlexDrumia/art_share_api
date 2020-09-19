034d918c4b3cfcd7838e29e5b6b505d94224d8bd
AlexDrumia <69897831+AlexDrumia@users.noreply.github.com>
2020-09-19 16:14:57 +0200

    Add new columns for favorites

---
+++ db/migrate/20200919083719_add_favorite_column_to_artworks_and_artwork_shares.rb
@@ -0,0 +1,6 @@
+class AddFavoriteColumnToArtworksAndArtworkShares < ActiveRecord::Migration[5.2]
+  def change
+    add_column :artworks, :favorite, :boolean, default: false, null: false
+    add_column :artwork_shares, :favorite, :boolean, default: false, null: false
+  end
+end
