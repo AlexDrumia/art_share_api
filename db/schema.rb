efa361903d1bef7f76a116c37c7ea542cd900af6
AlexDrumia <69897831+AlexDrumia@users.noreply.github.com>
2020-09-21 09:31:06 +0200

    Fix user being able to like something twice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_083719) do
+ActiveRecord::Schema.define(version: 2020_09_21_072306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_09_19_083719) do
     t.string "likeable_type", null: false
     t.bigint "likeable_id", null: false
     t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
+    t.index ["user_id", "likeable_type", "likeable_id"], name: "index_likes_on_user_id_and_likeable_type_and_likeable_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
